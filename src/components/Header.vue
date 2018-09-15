68baeb54a67ab99d038fd1907b873cab5bf6e0f8
Heitor <hbanhete@gmail.com>
2018-09-15 14:53:45 -0300

    Initial commit

---
+++ src/components/Header.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="header">
+        <h3>{{ name }}</h3>
+    </div>
+</template>
+
+
+<script>
+    import Vue from 'vue';
+
+    export default Vue.extend({
+        name: 'Header',
+        props: {
+            name: String,
+        }
+    })
+</script>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,19 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+      <Header name="Heitor e Laís"/>
   </div>
 </template>
+
+
+<script>
+    import Vue from 'vue';
+    import Header from '@/components/Header.vue';
+
+    export default Vue.extend({
+        name: 'about',
+        components: {
+            Header,
+        },
+    });
+</script>
