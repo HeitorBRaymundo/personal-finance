4404b5a54f00196c5110a2607d5bf201ad7e1655
Heitor <hbanhete@gmail.com>
2018-09-15 18:03:27 -0300

    added express to serve dist

---
+++ dist/precache-manifest.305a17761acf4cb8a94b87a13a952086.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "a76557c1134c0ecb309c",
+    "url": "/js/chunk-vendors.408a0b51.js"
+  },
+  {
+    "revision": "3737a7f003957ddbaabb",
+    "url": "/js/app.77293305.js"
+  },
+  {
+    "revision": "1eb8eb68dfe729aa9ab1",
+    "url": "/js/about.e154559c.js"
+  },
+  {
+    "revision": "25bf2ed7bdef4b3bd6b0c2f0d234aa72",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "3737a7f003957ddbaabb",
+    "url": "/css/app.4d0133c0.css"
+  }
+];
